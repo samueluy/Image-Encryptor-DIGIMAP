7966d73241f11f4ff1d42bea88ea900b0b3e709d
Samuel Uy <samuel_uy@dlsu.edu.ph>
2023-04-20 12:45:08 +0800

    For deployment:

--- main.py
+++ main.py
@@ -1,87 +1,85 @@
+# DIGIMAP - Group 14
+# Deculawan, Ryan Jay
+# Tan, Lance Griffin
+# Uy, Samuel Jedidiah
+# Yu, Ethan Angelo
+
 import streamlit as st
 import unimodular_chaos_encryption as uce
+import streamlit_toggle as tog  # pip install streamlit-toggle-switch
 from PIL import Image
+from io import BytesIO
 import numpy as np
 
-# from numpy import load
-# from numpy import expand_dims
-# from numpy import load
-# from numpy import expand_dims
-# import os
 
-# def encrypt(img, pass_1, pass_2):
-#     uce.encrypt(img, pass_1, pass_2)
-
-# def decrypt(img, pass_1, pass_2):
-#     uce.decrypt(img, pass_1, pass_2)
-
-# def encrypted_img(img):
-#     col1.write("Original Image :camera:")
-#     col1.image(image)
-
-    # encrypted_img = encrypt(img, pass_1, pass_2)
-    # col2.write("Encrypted Image :closed_lock_with_key:")
-    # st.sidebar.markdown("\n")
-    # st.sidebar.download_button("Download encrypted image", encrypted_img, "encrypted.png", "image/png")
+def convert_image(img):
+    buf = BytesIO()
+    img.save(buf, format="PNG")
+    byte_im = buf.getvalue()
+    return byte_im
 
 
-def main2(col1,col2,image_path):
+def run(col1, col2, image_path, option):
     image = Image.open(image_path)
     image_array = np.array(image)
+    col1.write("### Original Image :camera:")
+    col1.image(image)
 
-    pass_1_choices = uce.factors(image_array.size) # Array of keys for pass 1 - need to reset per upload
-    print(pass_1_choices)
-    print("\n\n")
-    pass_1 = int(st.selectbox('Choose a key:', pass_1_choices))
-    pass_2 = st.text_input("Enter your password")
-    
+    pass_1_choices = uce.factors(
+        image_array.size
+    )  # Array of keys for pass 1 - need to reset per upload
+    pass_1 = int(st.selectbox("Choose a key:", pass_1_choices))
+    pass_2 = st.text_input("Enter your password", type="password")
 
-    if(pass_1 != "" and pass_2 != ""):
-        pass_2 = float("0." + pass_2 + "1")                                                                                                         
-        print(pass_1, "type: ", type(pass_1))
-        print(pass_2, "type: ", type(pass_2))
+    if pass_1 != "" and pass_2 != "":
+        pass_2 = float("0." + pass_2 + "1")
 
         encrypted = uce.encrypt(image_path, pass_1, pass_2)
         image_array = np.array(encrypted)
-        pass_2_choices = uce.factors(image_array.size)
-        print(pass_2_choices)
-        print("\n\n")
 
-        col1.image(image)
+        if option:  # Encrypt
+            col2.write("### Encrypted Image :closed_lock_with_key:")
+            col2.image(encrypted)
+            downloadable = convert_image(encrypted)
+            st.sidebar.markdown("\n")
+            st.sidebar.download_button(
+                "Download encrypted image", downloadable, "encrypted.png", "image/png"
+            )
+        else:
+            decrypted = uce.decrypt(image, pass_1, pass_2)
+            col2.write("### Decrypted Image :unlock:")
+            col2.image(decrypted)
+            downloadable = convert_image(decrypted)
+            st.sidebar.markdown("\n")
+            st.sidebar.download_button(
+                "Download decrypted image", downloadable, "decrypted.png", "image/png"
+            )
 
-        #decrypted = uce.decrypt(encrypted, pass_1, pass_2)
-        col2.image(encrypted)
 
-
-    
 def main():
     st.set_page_config(layout="wide", page_title="")
-    st.write("## Image Encryptor")
-    st.write(
-        ## Add stuff here about the webapp lols
-    )
+    st.write("## Image Encryptor/Decryptor")
+
     st.sidebar.write("## Upload and download :gear:")
     col1, col2 = st.columns(2)
-    image_path = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
-    
+    image_path = st.sidebar.file_uploader("Upload image", type=["png", "jpg", "jpeg"])
+    option = tog.st_toggle_switch(
+        label="Encrypt", default_value=True, label_after=False
+    )
     if image_path is not None:
-        image = Image.open(image_path)
-        st.image(image_path, caption ="Input Image", use_column_width=True)
+        run(col1, col2, image_path, option)
+    st.write(
+        """Welcome to our :lock: Image Encryptor/Decryptor! This tool allows you to protect your images from prying :eyes: by encrypting them with a password of your choice.
 
-        main2(col1 ,col2, image_path)
+To use the tool, start by uploading the image :camera: you wish to encrypt or decrypt. Then, select a password :closed_lock_with_key: from the drop-down menu or enter your own password. Please make sure to remember the password you choose, as you will need it to decrypt the image later.
 
+Next, use the toggle switch :gear: to select whether you want to encrypt or decrypt the image. If you choose to encrypt the image, the tool will generate a new, encrypted version of the image that can only be viewed by someone who knows the password. If you choose to decrypt the image, the tool will use the password you entered to reveal the original image.
 
+Finally, you have the option to download the encrypted or decrypted image for safekeeping :inbox_tray:.
 
+Thank you for using our :lock: Image Encryptor/Decryptor to keep your images secure!"""
+    )
 
-    # image = Image.open("nature.png") # Temp
-    
-
-    
-    
-    # if image_path is not None:
-    #     encrypted_img(image)
-    # else:
-    #     encrypted_img("nature.png") # default 
 
 if __name__ == "__main__":
     main()
--- unimodular_chaos_encryption.py
+++ unimodular_chaos_encryption.py
@@ -1,13 +1,19 @@
+"""
+This implementation of the Unimodular Encryption using Logistic Maps algorithm 
+is adapted from the code written by Indra Bayu Muktyas, Sulistiawati, Samsul Arifin and 
+published on his GitHub repository in 2020. 
+The original code can be found at https://github.com/muktyas/encryption-unimodular-logistic-map.
+"""
+
 import numpy as np
 from PIL import Image
 import time
-from functools import reduce        # untuk faktor bilangan
+from functools import reduce
 import io
-#from matplotlib import pyplot as plt
 
 
-#OBE
-def r_ij(m, row_i, row_j, r):
+# OBE
+def elementary_row_operation(m, row_i, row_j, r):
     return m[row_i] + r * m[row_j]
 
 
@@ -15,21 +21,54 @@ def swap(m, row_i, row_j):
     m[row_i], m[row_j] = m[row_j], m[row_i]
 
 
-# logistic map sequence
+"""
+    This function generates a pseudo-random sequence of numbers using the logistic map equation with a given initial 
+    value and a number of iterations.
+    
+    :param x0: float - initial value for the logistic map equation
+    :param n: int - number of iterations for the logistic map equation
+    :return: ndarray - sequence of n pseudo-random numbers between 0 and 255
+    
+    The logistic map equation is defined as follows:
+    
+    x_{n+1} = r * x_n * (1 - x_n)
+    
+    where x_n is the value at iteration n, x_{n+1} is the value at iteration n+1, and r is a constant between 3.7 and 4.
+    In this implementation, r is fixed to 3.9.
+    
+    The function starts by iterating the logistic map equation for 1000 steps to ensure the sequence is not affected 
+    by the initial value. Then, it computes the sequence of pseudo-random numbers by iterating the logistic map 
+    equation n times and taking the integer part of the result multiplied by 1000 modulo 256.
+"""
+
+
 def log_map(x0, n):
     x = x0
     for i in range(1000):
-        x = 3.9 * x * (1 - x)  # 3.9 can be replaced with numbers in [3.7, 4]
+        x = 3.9 * x * (1 - x)
 
     seq = np.zeros(n, dtype=np.uint8)
     for i in range(n):
-        x = 3.9 * x * (1 - x)  # 3.9 can be replaced with numbers in [3.7, 4]
+        x = 3.9 * x * (1 - x)
         seq[i] = x * 1000 % 256
     return seq
 
 
+"""
+    Generates a key matrix and its inverse for encryption and decryption purposes.
+    Uses a logistic map to generate a sequence of numbers which is then used to fill an upper triangular matrix.
+    This matrix is then augmented with an identity matrix and transformed into an echelon form using OBE to obtain its inverse matrix.
+
+    Args:
+        n (int): Size of the matrix.
+        x0 (float): Initial value of the logistic map.
+
+    Returns:
+        tuple: A tuple containing two matrices - the key matrix and its inverse matrix.
+"""
+
+
 def generate_key(n, x0):
-    # upper triangular matrix
     count = n * (n - 1) // 2
     seq = log_map(x0, count + n - 1)
 
@@ -42,181 +81,186 @@ def generate_key(n, x0):
             idx += 1
 
     for i in range(1, n):
-        m[i] = r_ij(m, i, 0, seq[idx]) % 256
+        m[i] = elementary_row_operation(m, i, 0, seq[idx]) % 256
         idx += 1
 
     augmented = np.zeros((n, 2 * n))
     augmented[:, :n] = m
     augmented[:, n:] = np.eye(n)
 
-    # OBE for obtaining the inverse
-    # Lower-triangular matrix
     for col in range(n):
         for row in range(col + 1, n):
-            augmented[row] = r_ij(augmented, row, col, -augmented[row, col]) % 256
+            augmented[row] = (
+                elementary_row_operation(augmented, row, col, -augmented[row, col])
+                % 256
+            )
 
-    # Upper-triangular matrix
     for col in range(1, n):
         for row in range(col):
-            augmented[row] = r_ij(augmented, row, col, -augmented[row, col]) % 256
+            augmented[row] = (
+                elementary_row_operation(augmented, row, col, -augmented[row, col])
+                % 256
+            )
 
     inverse = augmented[:, n:]
 
     return m, inverse
 
 
+"""
+    Encrypts an image using a given key.
+    The function takes the path to an image file 'image_path', along with the size 
+    'size' and the initial value 'x0' used to generate the encryption key. It then 
+    loads the image file and converts it to a NumPy array, generates the encryption 
+    key using the 'generate_key()' function, reshapes the image array, multiplies 
+    the key and the image array, reshapes the resulting cipher, and converts it to 
+    a Pillow Image object. The encrypted image is returned.
+
+    Args:
+    - image_path (str): The path to the image file.
+    - size (int): The size of the image.
+    - x0 (int): The initial value used to generate the encryption key.
+
+    Returns:
+    - PIL.Image.Image: The encrypted image as a Pillow Image object.
+"""
+
+
 def encrypt(image_path, size, x0):
+    import time
+    import numpy as np
+    from PIL import Image
+
     start_time = time.time()
-    
+
     # Load the image
     image = Image.open(image_path)
     image_array = np.array(image)
-    
+
     # Generate the key
     key, inverse_key = generate_key(size, x0)
-    
+
     # Reshape the image
-    image_reshaped = image_array.reshape((size, image_array.size//size))
-    
+    image_reshaped = image_array.reshape((size, image_array.size // size))
+
     # Multiply the key and the image
     multiplied = np.dot(key, image_reshaped) % 256
-    
+
     # Reshape the cipher
-    cipher = multiplied.reshape(image_array.shape).astype(np.uint8)
-    
-    # Save the cipher
-    cipher_image = Image.fromarray(cipher)
-    bytes_image = io.BytesIO()
+    cipher_array = multiplied.reshape(image_array.shape).astype(np.uint8)
+
+    # Convert the cipher array to a Pillow Image object and return it
+    cipher_image = Image.fromarray(cipher_array)
     return cipher_image
-    # cipher_image.save('encrypted.png')
+
+
+"""
+    Decrypts an image using a given key.
+
+    The function takes a grayscale image 'gb' as input, along with its size 'size' 
+    and the initial value 'x0' used to generate the encryption key. It then reshapes 
+    the image array and applies the inverse of the encryption key to decrypt the image. 
+    The resulting decrypted image is converted to a Pillow Image object and returned.
     
-    # print("-----------------------------------------------------------------")
-    # print('Your encrypted image is called "encrypted.png" in the same folder.')
-    # print('The encryption time is', time.time() - start_time, 'seconds.')
+    Args:
+    - gb (numpy.ndarray): A grayscale image represented as a NumPy array.
+    - size (int): The size of the image.
+    - x0 (int): The initial value used to generate the encryption key.
     
-    # # Get the second layer password
-    # password2 = str(x0)
-    # password2_length = len(password2)
-    # password2 = password2[2:password2_length - 1]
-    
-    # print('Please remember your two-layer password:')
-    # print('Password 1:', size)
-    # print('Password 2:', password2)
-    # print("-----------------------------------------------------------------")
+    Returns:
+    - PIL.Image.Image: The decrypted image as a Pillow Image object.
+"""
+
 
 def decrypt(gb, size, x0):
+    import numpy as np
+    from PIL import Image
+
     print("Decryption process begin.")
-    start_time = time.time()
-    #gbku = Image.open(gb)
-    mgb = np.array(gb)
+
+    # Convert the grayscale image to a NumPy array
+    image_array = np.array(gb)
+
+    # Generate the decryption key using the 'generate_key()' function
+    key, inverse = generate_key(size, x0)
+
+    # Reshape the image array
+    image_array_reshaped = image_array.reshape((size, image_array.size // size))
+
+    # Apply the inverse of the encryption key to decrypt the image
+    decrypted_image_array = np.dot(inverse, image_array_reshaped) % 256
+    decrypted_image_array_reshaped = decrypted_image_array.reshape(
+        image_array.shape
+    ).astype(np.uint8)
+
+    # Convert the decrypted image array to a Pillow Image object and return it
+    decrypted_image = Image.fromarray(decrypted_image_array_reshaped)
+    return decrypted_image
+
+
+"""
+    Compares two image files to determine if they are the same.
+    The function takes the file paths of two image files, 'original_image' and 'decrypted_image', 
+    opens them with the Pillow Image module, and converts them to NumPy arrays. 
+    It then uses the 'all()' method of NumPy arrays to compare the two images, 
+    returning "Both images are the same." if they are identical, and "Both images are different." otherwise.
     
-    kunciku, balikku = generate_key(size, x0)
+    Args:
+    - original_image (str): The file path of the original image.
+    - decrypted_image (str): The file path of the decrypted image to be compared.
     
-    mgb_reshape = mgb.reshape((size, mgb.size//size))
-    
-    m_kali = np.dot(balikku, mgb_reshape)%256
-    m_decipher = m_kali.reshape(mgb.shape).astype(np.uint8)
-    
-    decipher = Image.fromarray(m_decipher)
-    return decipher
-    # decipher.save('decryption.png')
-    # print('Your decrypted image is called "decryption.png" in the same folder.')
-    # print('The decryption time is ', time.time() - start_time, ' seconds.')
+    Returns:
+    - str: A string indicating whether the images are the same or different.
+"""
 
 
 def check_same(original_image, decrypted_image):
-    gba = Image.open(original_image)
-    gbd = Image.open(decrypted_image)
-    
-    mgba = np.array(gba)
-    mgbd = np.array(gbd)
-    
-    if (mgba==mgbd).all():
+    from PIL import Image
+    import numpy as np
+
+    # Open the original and decrypted images with the Pillow Image module
+    image_a = Image.open(original_image)
+    image_b = Image.open(decrypted_image)
+
+    # Convert the images to NumPy arrays
+    image_a_array = np.array(image_a)
+    image_b_array = np.array(image_b)
+
+    # Compare the two arrays using the 'all()' method
+    if (image_a_array == image_b_array).all():
         return "Both images are the same."
     else:
         return "Both images are different."
 
 
+"""
+    Returns a list of all factors of the input integer 'n' up to a maximum of 1000.
+    The function uses a set comprehension to generate all factors of 'n' in a raw format, 
+    then sorts the factors in ascending order and filters out any factors greater than 1000.
+
+    Args:
+    - n (int): The integer to find factors of.
+
+    Returns:
+    - List[int]: A list of all factors of 'n' up to a maximum of 1000, sorted in ascending order.
+"""
+
+
 def factors(n):
     from functools import reduce
-    fkt = set(reduce(list.__add__, 
-                ([i, n//i] for i in range(2, int(n**0.5) + 1) if n % i == 0)))
-    fkt_list = sorted(fkt)
-    return [i for i in fkt_list if i < 1000]
-##########################################################
+
+    # Use set comprehension to generate all factors of 'n' in a raw format
+    raw_factors = set(
+        reduce(
+            list.__add__,
+            ([i, n // i] for i in range(2, int(n**0.5) + 1) if n % i == 0),
+        )
+    )
+
+    # Sort the factors in ascending order and filter out any factors greater than 1000
+    sorted_factors = sorted(raw_factors)
+    return [i for i in sorted_factors if i < 1000]
 
 
-# def run(img, pass_1, pass_2):
-#     print("============================================================================")
-#     print("Digital Image Encryption Algorithm Trough Unimodular Matrix and Logistic Map")
-#     print("============================================================================")
-#     jawaban = input("Please select, you want to encrypt (e) or decrypt (d):")
-#     if jawaban == "e" or jawaban == "E":
-#         print("-------------------------")
-#         print("You will encrypt an image")
-#         print("-------------------------")
-#         print()
-#         gb = input("Now, insert your image with its extension (example: image.png):")
-#         gbku = Image.open(gb)
-#         mgb = np.array(gbku)
-#         ukuran = mgb.size
-#         faktor = factors(ukuran)
-
-#         # print ukuran
-#         print()
-#         print("----------------------------------------------------------------------------")
-#         print(faktor)
-#         print("----------------------------------------------------------------------------")
-
-#         password1 = int(input("Please choose one of the numbers in the set above as Password 1:"))
-#         # print password1
-#         while password1 not in faktor:
-#             print("Your choice are ", password1)
-#             print("Not in the given list.")
-#             print("-" * 50, "\nThe list of numbers are:")
-#             print(faktor)
-#             password1 = int(input("Please choose again the right one:"))
-#         print("Your Password 1 is: ", password1, "\n")
-#         pw2 = input("Enter Password 2 in the form of a number, maximum 14 digits (example: 22021985)\nPassword 2:")
-#         print("Your Password 2 is: ", pw2)
-#         password2 = float("0." + pw2 + "1")
-
-#         # proses enkripsi dimulai
-#         encrypt(img, pass_1, pass)
-
-
-#     else:
-#         print("*" * 80)
-#         print("Digital Image Decryption Algorithm Through Unimodular Matrix and Logistic Map")
-#         print("-" * 50)
-#         print()
-
-#         gb = input("Now, insert your encrypted image with its extension (example: image.png):")
-#         gbku = Image.open(gb)
-#         mgb = np.array(gbku)
-#         ukuran = mgb.size
-#         faktor = sorted(factors(ukuran))
-
-#         # print ukuran
-#         print("----------------------------------------------------------------------------")
-#         print(faktor)
-#         print("----------------------------------------------------------------------------")
-
-#         password1 = int(input("Please choose one of the numbers in the set above as Password 1:"))
-#         # print password1
-#         while password1 not in faktor:
-#             print("Your choice is ", password1)
-#             print("not in the given list.")
-#             print("-" * 50, "\nThe list of numbers are:")
-#             print(faktor)
-#             password1 = int(input("Please choose again the right one:"))
-#         print("Your Password 1 is: ", password1, "\n")
-#         pw2 = input("Enter Password 2 in the form of a number, maximum 14 digits (example: 22021985)\nPassword 2:")
-#         print("Your Password 2 is: ", pw2)
-#         password2 = float("0." + pw2 + "1")
-
-#         # decryption process begins
-#         decrypt(gb, password1, password2)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     pass
